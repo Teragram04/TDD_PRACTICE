808dddadce4f4fd196e0dbcf6f903d57a513709f
Teragram04 <91078893+Teragram04@users.noreply.github.com>
2025-10-24 12:46:22 -0500

    Update test_Virtual_Store.py

--- test_Virtual_Store.py
+++ test_Virtual_Store.py
@@ -1,7 +1,8 @@
 from Virtual_Store import Item
 
-def test_item(self):
-  item = Virtual_Store.get_item(name, count, price)
+def test_item_price():
+  item_price = Item.get_price(price)
+  
    
     
 def test_view_cart():
